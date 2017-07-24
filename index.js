7c3972d795ffd03f93585b9b4aacff3c60ed5754
Su Yijia <crystalremnet@gmail.com>
2017-07-24 15:13:45 +0800

    Init Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var redis = require("redis");
+var sub = redis.createClient(), pub = redis.createClient();
+var crypto = require('crypto');
+
+app.get('/query', function(req, res){
+  var rand_hash = crypto.randomBytes(20).toString('hex');
+  if (req.query.url) {
+    var pub_msg = "GET_EXT_INFO_TASK" + "^^^^" + rand_hash + "^^^^" + req.query.url
+    pub.publish("GetExtInfo", pub_msg);
+  }
+  else {
+    res.send('{"status":"error", "reason":"no url param"}');
+  }
+  sub.subscribe("GetExtInfoReply");
+  sub.on("message", function (channel, message) {
+    console.log(message);
+    var replyJSON = JSON.parse(message);
+    if (replyJSON['task_id'] == rand_hash) {
+      res.send(message);
+    }
+  });
+});
+
+app.listen(3000);
+console.log("HTTP Server Listening...")
